299417847a15f6dd0743904fa779a83e4dd59113
Mckenzie <rduser_1@outlookcom>
2025-05-06 12:09:29 +0000

    fix: uncomment base path configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // base: process.env.VITE_BASE_PATH || "Pentagon-Wifi-Onboarding",
+  base: process.env.VITE_BASE_PATH || "Pentagon-Wifi-Onboarding",
 }));
