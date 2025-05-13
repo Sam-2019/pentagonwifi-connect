6be024f41e70d4614632ba99359c3525115f379f
Sam <samtmartey@gmail.com>
2025-05-13 12:49:16 +0000

    feat: integrate react-tailwindcss-datepicker for date selection in RegistrationForm

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,8 @@ export default {
 		"./components/**/*.{ts,tsx}",
 		"./app/**/*.{ts,tsx}",
 		"./src/**/*.{ts,tsx}",
+		"./src/**/*.{js,jsx,ts,tsx}",
+        "./node_modules/react-tailwindcss-datepicker/dist/index.esm.{js,ts}",
 	],
 	prefix: "",
 	theme: {
