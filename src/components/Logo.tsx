c59e048069b441271580159d7c251b153638e0a7
Mckenzie <rduser_1@outlookcom>
2025-05-13 18:35:21 +0000

    fix: update logo image source path in Logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ const Logo: React.FC = () => {
   return (
     <div className="flex items-center justify-center">
       <img 
-        src="src/assets/logo.png" 
+        src="./public/logo.png" 
         alt="Pentagon WiFi Logo" 
         className="mt-10 transition-transform duration-300 hover:scale-105 object-cover w-48 md:w-48 h-auto"    
       />
