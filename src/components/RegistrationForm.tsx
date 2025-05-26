5fed1e567519d7aa9df90cd4cfa4adb3f731d3d1
Mckenzie <rduser_1@outlookcom>
2025-05-26 21:05:34 +0000

    fix: update Google Scripts URL handling in RegistrationForm component

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -129,6 +129,10 @@ const RegistrationForm: React.FC = () => {
   const totalCost = registrationFee + planFee;
 
   const onSubmit = async (data: FormData) => {
+    const googleScriptUrl =
+    import.meta.env.VITE_ENV === "development"
+      ? import.meta.env.VITE_GOOGLE_SCRIPTS_TEST
+      : import.meta.env.VITE_GOOGLE_SCRIPTS_LIVE;
     const config = {
       reference: new Date().getTime().toString(),
       email: data.email.toString(),
@@ -198,7 +202,7 @@ const RegistrationForm: React.FC = () => {
       };
 
       toast.promise(
-        fetch(import.meta.env.VITE_GOOGLE_SCRIPTS_URL, {
+        fetch(googleScriptUrl, {
           method: "POST",
           mode: "no-cors",
           body: JSON.stringify(payload),
