772b0053fea9cd3fc9703b7a24cd00951c263ea2
Mckenzie <rduser_1@outlookcom>
2025-05-27 19:00:34 +0000

    feat: implement Paystack payment integration with configuration and success handling

---
+++ src/hooks/use-paystack.tsx
@@ -0,0 +1,54 @@
+import { PaymentInfo, PaystackSuccessReference, DbPayload } from "@/lib/utils";
+import { usePaystackPayment } from "react-paystack";
+
+export const paystackPay = (paymentInfo: PaymentInfo) => {
+  const config = {
+    reference: paymentInfo.clientReference,
+    email: paymentInfo.email,
+    amount: paymentInfo.totalCost,
+    publicKey: import.meta.env.VITE_STACK_COLLECTIONS,
+    currency: import.meta.env.VITE_CURRENCY,
+    metadata: {
+      custom_fields: [
+        {
+          display_name: "Full Name",
+          variable_name: "fullName",
+          value: String(paymentInfo.fullName),
+        },
+        {
+          display_name: "Phone Number",
+          variable_name: "phoneNumber",
+          value: String(paymentInfo.phoneNumber),
+        },
+        {
+          display_name: "Subscription Plan",
+          variable_name: "subscriptionPlan",
+          value: String(paymentInfo.subscriptionPlan.toUpperCase()),
+        },
+      ],
+    },
+  };
+
+  const onSuccess = (reference: PaystackSuccessReference): void => {
+    console.log("Payment successful: ", reference);
+    const payload: DbPayload = {
+      ...paymentInfo,
+      reference: reference.reference,
+      transaction: reference.transaction,
+      status: reference.status,
+      message: reference.message,
+    };
+  };
+
+  const onClose = () => {};
+  const initializePayment = usePaystackPayment(config);
+
+  return {
+    initialize: () => {
+      initializePayment({
+        onSuccess,
+        onClose,
+      });
+    },
+  };
+};
