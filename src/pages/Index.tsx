22213c94f8c95ef084d6018bd189dd261e7513f3
Mckenzie <rduser_1@outlookcom>
2025-05-28 18:43:02 +0000

    feat: add ScanMeModal component and integrate QR code button in Index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,19 +1,29 @@
 import RegistrationForm from "@/components/RegistrationForm";
 import LightningBackground from "@/components/WaveBackground";
 import Logo from "@/components/Logo";
+import { QrCode } from "lucide-react";
+import { useState } from "react";
+import ScanMeModal from "@/components/ScanMe";
 
 const Index = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const onClose = () => {
+    setIsModalOpen(false);
+  };
+  const onOpen = () => {
+    setIsModalOpen(true);
+  };
+
   return (
     <div>
       <LightningBackground />
-      <header className="w-full text-center text-sm text-gray-500 z-10">
-        <div className="max-w-7xl mx-auto mb-3">
-          <Logo />
-        </div>
+      <header className="w-full mx-auto py-7 bg-white shadow-sm">
+        <Logo />
       </header>
 
-      <main className="flex-1 flex flex-col items-center justify-center z-10">
-        <div className="text-center">
+      <main className="flex-1 flex flex-col items-center justify-center">
+        <div className="text-center mt-10">
           <h1 className="text-3xl md:text-4xl font-bold text-primary mb-3">
             Connect to Pentagon WiFi
           </h1>
@@ -28,9 +38,20 @@ const Index = () => {
         </div>
       </main>
 
+      <div className="fixed bottom-4 right-4 hidden md:block">
+        <button className=" rounded-full shadow-lg" onClick={onOpen}>
+          <QrCode className="h-20 w-20 text-blue-900 mx-auto p-3" />
+        </button>
+      </div>
+
       <footer className="w-full py-6 text-center text-sm text-gray-500">
-        <p>© {new Date().getFullYear()} Pentagon WiFi - Powered by Kuanos Cloud. All rights reserved.</p>
+        <p>
+          © {new Date().getFullYear()} Pentagon WiFi - Powered by Kuanos Cloud.
+          All rights reserved.
+        </p>
       </footer>
+
+      <ScanMeModal open={isModalOpen} onClose={onClose} />
     </div>
   );
 };
