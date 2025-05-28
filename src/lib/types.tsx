ec63562909a9ca0dbf397e7c4fbabee10a0c3db1
Mckenzie <rduser_1@outlookcom>
2025-05-28 12:58:29 +0000

    refactor: standardize PaymentInfo and DbPayload interfaces by adding missing fields and ensuring consistency

--- src/lib/types.tsx
+++ src/lib/types.tsx
@@ -1,72 +1,69 @@
-
 export interface FormData {
-    fullName: string;
-    dateOfBirth: Date;
-    phoneNumber: string;
-    email: string;
-    blockCourt: string;
-    roomType: string;
-    roomNumber: string;
-    subscriptionPlan: string;
-    isCustodian: boolean;
-  }
+  fullName: string;
+  dateOfBirth: Date;
+  phoneNumber: string;
+  email: string;
+  blockCourt: string;
+  roomType: string;
+  roomNumber: string;
+  subscriptionPlan: string;
+  isCustodian: boolean;
+}
+
+export interface Payload {
+  fullName: string;
+  dateOfBirth: string;
+  phoneNumber: string;
+  email: string;
+  blockCourt: string;
+  roomType: string;
+  roomNumber: string;
+  subscriptionPlan: string;
+  isCustodian: boolean;
+  totalCost: string;
+  dateTime: string;
+}
+
+export interface PaystackSuccessReference {
+  reference: string;
+  transaction: string;
+  status: string;
+  message: string;
+}
+
+export interface PaymentInfo {
+  fullName: string;
+  phoneNumber: string;
+  subscriptionPlan: string;
+  planFee: number;
+  registrationFee: number;
+  totalCost: number;
+  clientReference: string;
+  email: string;
+  dateOfBirth: Date;
+  blockCourt: string;
+  roomType: string;
+  roomNumber: string;
+  isCustodian: boolean;
+  dateTime: Date;
+}
+
+export interface DbPayload {
+  fullName: string;
+  phoneNumber: string;
+  subscriptionPlan: string;
+  planFee: number;
+  registrationFee: number;
+  totalCost: number;
+  clientReference: string;
+  email: string;
+  dateOfBirth: Date;
+  blockCourt: string;
+  roomType: string;
+  roomNumber: string;
+  isCustodian: boolean;
+  dateTime: Date;
+
+  providerResponse: object;
   
-  export interface Payload {
-    fullName: string;
-    dateOfBirth: string;
-    phoneNumber: string;
-    email: string;
-    blockCourt: string;
-    roomType: string;
-    roomNumber: string;
-    subscriptionPlan: string;
-    isCustodian: boolean;
-    totalCost: string;
-    dateTime: string;
-  }
-  
-  export interface PaystackSuccessReference {
-    reference: string;
-    transaction: string;
-    status: string;
-    message: string;
-  }
-  
-  export interface PaymentInfo {
-    fullName: string;
-    phoneNumber: string;
-    subscriptionPlan: string;
-    planFee: number;
-    amount: number;
-    clientReference: string;
-  
-    email?: string;
-    dateOfBirth?: Date;
-    blockCourt?: string;
-    roomType?: string;
-    roomNumber?: string;
-    isCustodian?: boolean;
-    totalCost?: number;
-  }
-  
-  export interface DbPayload {
-    fullName: string;
-    phoneNumber: string;
-    subscriptionPlan: string;
-    planFee: number;
-    amount: number;
-    clientReference: string;
-  
-    email?: string;
-    dateOfBirth?: Date;
-    blockCourt?: string;
-    roomType?: string;
-    roomNumber?: string;
-    isCustodian?: boolean;
-    totalCost?: number;
-  
-    status: string;
-    message: string;
-    reference: string;
-    transaction: string;
-  }
+}
