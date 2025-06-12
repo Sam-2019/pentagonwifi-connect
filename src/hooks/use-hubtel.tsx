6d2d1d73d6a972079b532d8e32aeff41d38d9eca
Sam-2019 <samtmartey@gmail.com>
2025-06-12 07:28:21 +0000

    cleanup

--- src/hooks/use-hubtel.tsx
+++ src/hooks/use-hubtel.tsx
@@ -4,132 +4,132 @@ import CheckoutSdk from "@hubteljs/checkout";
 import { v4 as uuidv4 } from "uuid";
 
 export const hubtelPay = (paymentInfo: PaymentInfo) => {
-	const checkout = new CheckoutSdk();
+  const checkout = new CheckoutSdk();
 
-	const reference = String(uuidv4());
-	const slicedReference = reference.slice(0, 8);
-	const clientReference = `PWT-${slicedReference}`;
-	const credentials = JSON.parse(paymentInfo.credentials);
-	const userName = credentials.userName;
+  const reference = String(uuidv4());
+  const slicedReference = reference.slice(0, 8);
+  const clientReference = `PWT-${slicedReference}`;
+  const credentials = JSON.parse(paymentInfo.credentials);
+  const userName = credentials.userName;
 
-	const registrationType = paymentInfo.registrationType;
-	const purchaseDescription = `Payment of GHS ${
-		paymentInfo.planFee
-	} PENTAGONWIFI ${paymentInfo.subscriptionPlan.toUpperCase()} data package for (${paymentInfo.fullName.toUpperCase()}-${
-		paymentInfo.phoneNumber
-	})`;
+  const registrationType = paymentInfo.registrationType;
+  const purchaseDescription = `Payment of GHS ${
+    paymentInfo.planFee
+  } PENTAGONWIFI ${paymentInfo.subscriptionPlan.toUpperCase()} data package for (${paymentInfo.fullName.toUpperCase()}-${
+    paymentInfo.phoneNumber
+  })`;
 
-	const topupDescription = `Top-up of GHS ${
-		paymentInfo.planFee
-	} PENTAGONWIFI ${paymentInfo.subscriptionPlan.toUpperCase()} data package for (${userName}-${
-		paymentInfo.phoneNumber
-	})`;
+  const topupDescription = `Top-up of GHS ${
+    paymentInfo.planFee
+  } PENTAGONWIFI ${paymentInfo.subscriptionPlan.toUpperCase()} data package for (${userName}-${
+    paymentInfo.phoneNumber
+  })`;
 
-	const purchaseInfo = {
-		amount: paymentInfo.totalCost,
-		purchaseDescription:
-			registrationType === "registration"
-				? purchaseDescription
-				: topupDescription,
-		customerPhoneNumber: `'${paymentInfo.phoneNumber}`,
-		clientReference: clientReference,
-	};
+  const purchaseInfo = {
+    amount: paymentInfo.totalCost,
+    purchaseDescription:
+      registrationType === "registration"
+        ? purchaseDescription
+        : topupDescription,
+    customerPhoneNumber: `'${paymentInfo.phoneNumber}`,
+    clientReference: clientReference,
+  };
 
-	const stringifyPurchaseInfo = JSON.stringify(purchaseInfo);
+  const stringifyPurchaseInfo = JSON.stringify(purchaseInfo);
 
-	const config = {
-		callbackUrl: import.meta.env.VITE_CALLBACK_URL,
-		merchantAccount: import.meta.env.VITE_MERCHANT,
-		basicAuth: import.meta.env.VITE_BASIC_AUTH,
-	};
+  const config = {
+    callbackUrl: import.meta.env.VITE_CALLBACK_URL,
+    merchantAccount: import.meta.env.VITE_MERCHANT,
+    basicAuth: import.meta.env.VITE_BASIC_AUTH,
+  };
 
-	return {
-		initialize: (
-			toast: {
-				promise: (
-					promise: Promise<unknown>,
-					options: { loading: string; success: string; error: string },
-				) => void;
-			},
-			setIsSuccessModalOpen: (value: boolean) => void,
-			reset: () => void,
+  return {
+    initialize: (
+      toast: {
+        promise: (
+          promise: Promise<unknown>,
+          options: { loading: string; success: string; error: string }
+        ) => void;
+      },
+      setIsSuccessModalOpen: (value: boolean) => void,
+      reset: () => void,
 
-			setDatePickerValue?: (value: {
-				startDate: Date | null;
-				endDate: Date | null;
-			}) => void,
-		) => {
-			checkout.openModal({
-				purchaseInfo: purchaseInfo,
-				config: config,
-				callBacks: {
-					onInit: () => console.log("Iframe initialized: "),
-					onPaymentSuccess: (data) => {
-						console.log("Payment successful: ", data.data);
-						const response = data.data;
-						const stringifyResponse = JSON.stringify(response);
+      setDatePickerValue?: (value: {
+        startDate: Date | null;
+        endDate: Date | null;
+      }) => void
+    ) => {
+      checkout.openModal({
+        purchaseInfo: purchaseInfo,
+        config: config,
+        callBacks: {
+          onInit: () => console.log("Iframe initialized: "),
+          onPaymentSuccess: (data) => {
+            console.log("Payment successful: ", data.data);
+            const response = data.data;
+            const stringifyResponse = JSON.stringify(response);
 
-						const dbInfo: DbPayload = {
-							...paymentInfo,
-							purchaseInfo: stringifyPurchaseInfo,
-							clientReference: clientReference,
-							providerResponse: stringifyResponse,
-						};
-						checkout.closePopUp();
-						toast.promise(
-							fetch(`${baseUrl}/api/register/sale`, {
-								method: "POST",
-								mode: "cors",
-								body: JSON.stringify(dbInfo),
-								headers: {
-									"Content-Type": "application/json",
-								},
-							}).then(() => {
-								setTimeout(() => setIsSuccessModalOpen(true), 300);
-								reset();
+            const dbInfo: DbPayload = {
+              ...paymentInfo,
+              purchaseInfo: stringifyPurchaseInfo,
+              clientReference: clientReference,
+              providerResponse: stringifyResponse,
+            };
+            checkout.closePopUp();
+            toast.promise(
+              fetch(`${baseUrl}/api/register/sale`, {
+                method: "POST",
+                mode: "cors",
+                body: JSON.stringify(dbInfo),
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }).then(() => {
+                setTimeout(() => setIsSuccessModalOpen(true), 300);
+                reset();
 
-								setDatePickerValue({
-									startDate: null,
-									endDate: null,
-								});
-							}),
-							{
-								loading: "Connecting you to Pentagon WiFi...",
-								success: "Registration complete!",
-								error: "Registration failed. Please try again.",
-							},
-						);
-					},
-					onPaymentFailure: (data) => {
-						console.log("Payment failed: ", data);
-					},
-					onLoad: () => {
-						console.log("Checkout has been loaded: ");
-					},
-					onFeesChanged: (fees) => {
-						console.log("Payment channel has changed: ", fees);
-					},
-					onResize: (size) => {
-						console.log("Iframe has been resized: ", size?.height);
-					},
-					onClose: () => {
-						const dbInfo: DbPayload = {
-							...paymentInfo,
-							purchaseInfo: stringifyPurchaseInfo,
-							clientReference: clientReference,
-							providerResponse: "N/A",
-						};
-						fetch(`${baseUrl}/api/register/sale/intent`, {
-							method: "POST",
-							mode: "cors",
-							body: JSON.stringify(dbInfo),
-							headers: {
-								"Content-Type": "application/json",
-							},
-						}).then(() => {});
-					},
-				},
-			});
-		},
-	};
+                setDatePickerValue({
+                  startDate: null,
+                  endDate: null,
+                });
+              }),
+              {
+                loading: "Connecting you to Pentagon WiFi...",
+                success: "Registration complete!",
+                error: "Registration failed. Please try again.",
+              }
+            );
+          },
+          onPaymentFailure: (data) => {
+            console.log("Payment failed: ", data);
+          },
+          onLoad: () => {
+            console.log("Checkout has been loaded: ");
+          },
+          onFeesChanged: (fees) => {
+            console.log("Payment channel has changed: ", fees);
+          },
+          onResize: (size) => {
+            console.log("Iframe has been resized: ", size?.height);
+          },
+          onClose: () => {
+            const dbInfo: DbPayload = {
+              ...paymentInfo,
+              purchaseInfo: stringifyPurchaseInfo,
+              clientReference: clientReference,
+              providerResponse: "N/A",
+            };
+            fetch(`${baseUrl}/api/register/sale/intent`, {
+              method: "POST",
+              mode: "cors",
+              body: JSON.stringify(dbInfo),
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }).then(() => {});
+          },
+        },
+      });
+    },
+  };
 };
