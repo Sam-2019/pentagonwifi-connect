4d65e62f2ba9124294c4f2daaa14946949670fb6
Sam-2019 <samtmartey@gmail.com>
2025-06-13 07:19:10 +0000

    payment initilaize changes

--- src/hooks/use-hubtel.tsx
+++ src/hooks/use-hubtel.tsx
@@ -1,139 +1,142 @@
 import type {
-	PaymentInfo,
-	PendingRegistrationPayload,
-	SalesPayload,
+  PendingPaymentPayload,
+  SalesPayload,
+  UserInfo,
 } from "@/lib/types";
-import { writePendingRegistration, writeSale } from "@/lib/utils";
+import {
+  writePendingRegistration,
+  writeRegistration,
+  writeSale,
+} from "@/lib/utils";
 import CheckoutSdk from "@hubteljs/checkout";
 import { v4 as uuidv4 } from "uuid";
 
-export const hubtelPay = (paymentInfo: PaymentInfo, regID: string) => {
-	const checkout = new CheckoutSdk();
+export const hubtelPay = (userInfo: UserInfo) => {
+  const checkout = new CheckoutSdk();
 
-	const reference = String(uuidv4());
-	const slicedReference = reference.slice(0, 8);
-	const clientReference = `PWT-${slicedReference}`;
-	const credentials = JSON.parse(paymentInfo.credentials);
-	const userName = credentials.userName;
+  const reference = String(uuidv4());
+  const slicedReference = reference.slice(0, 8);
+  const clientReference = `PWT-${slicedReference}`;
+  const credentials = JSON.parse(userInfo.credentials);
+  const userName = credentials.userName;
 
-	const registrationType = paymentInfo.registrationType;
-	const purchaseDescription = `Payment of GHS ${
-		paymentInfo.planFee
-	} PENTAGONWIFI ${paymentInfo.subscriptionPlan.toUpperCase()} data package for (${paymentInfo.fullName.toUpperCase()}-${
-		paymentInfo.phoneNumber
-	})`;
+  const registrationType = userInfo.registrationType;
+  const purchaseDescription = `Payment of GHS ${
+    userInfo.planFee
+  } PENTAGONWIFI ${userInfo.subscriptionPlan.toUpperCase()} data package for (${userInfo.fullName.toUpperCase()}-${
+    userInfo.phoneNumber
+  })`;
 
-	const topupDescription = `Top-up of GHS ${
-		paymentInfo.planFee
-	} PENTAGONWIFI ${paymentInfo.subscriptionPlan.toUpperCase()} data package for (${userName}-${
-		paymentInfo.phoneNumber
-	})`;
+  const topupDescription = `Top-up of GHS ${
+    userInfo.planFee
+  } PENTAGONWIFI ${userInfo.subscriptionPlan.toUpperCase()} data package for (${userName}-${
+    userInfo.phoneNumber
+  })`;
 
-	const purchaseInfo = {
-		amount: paymentInfo.totalCost,
-		purchaseDescription:
-			registrationType === "registration"
-				? purchaseDescription
-				: topupDescription,
-		customerPhoneNumber: `'${paymentInfo.phoneNumber}`,
-		clientReference: clientReference,
-	};
+  const purchaseInfo = {
+    amount: userInfo.totalCost,
+    purchaseDescription:
+      registrationType === "registration"
+        ? purchaseDescription
+        : topupDescription,
+    customerPhoneNumber: `'${userInfo.phoneNumber}`,
+    clientReference: clientReference,
+  };
 
-	const stringifyPurchaseInfo = JSON.stringify(purchaseInfo);
+  const stringifyPurchaseInfo = JSON.stringify(purchaseInfo);
 
-	const config = {
-		callbackUrl: import.meta.env.VITE_CALLBACK_URL,
-		merchantAccount: import.meta.env.VITE_MERCHANT,
-		basicAuth: import.meta.env.VITE_BASIC_AUTH,
-	};
+  const config = {
+    callbackUrl: import.meta.env.VITE_CALLBACK_URL,
+    merchantAccount: import.meta.env.VITE_MERCHANT,
+    basicAuth: import.meta.env.VITE_BASIC_AUTH,
+  };
 
-	return {
-		initialize: (
-			toast: {
-				promise: (
-					promise: Promise<unknown>,
-					options: { loading: string; success: string; error: string },
-				) => void;
-			},
-			setIsSuccessModalOpen: (value: boolean) => void,
-			reset: () => void,
-			setDatePickerValue: (value: {
-				startDate: Date | null;
-				endDate: Date | null;
-			}) => void,
-			setRegID: (value: string) => void,
-		) => {
-			checkout.openModal({
-				purchaseInfo: purchaseInfo,
-				config: config,
-				callBacks: {
-					onInit: () => console.log("Iframe initialized: "),
-					onPaymentSuccess: (data) => {
-						console.log("Payment successful: ", data.data);
-						const response = data.data;
+  const checkoutInfo: PendingPaymentPayload = {
+    ...userInfo,
+    purchaseInfo: stringifyPurchaseInfo,
+    clientReference: clientReference,
+  };
 
-						const stringifyResponse = JSON.stringify(response);
-						const parseResponse = JSON.parse(response);
-						const transactionId = parseResponse.transactionId;
-						const externalTransactionId = parseResponse.externalTransactionId;
+  const response = writeRegistration(checkoutInfo);
+  response
+    .then((res) => {})
+    .catch((err) => console.log(err))
+    .finally(() => {});
 
-						const dbInfo: SalesPayload = {
-							...paymentInfo,
-							regID: regID,
-							purchaseInfo: stringifyPurchaseInfo,
-							clientReference: clientReference,
-							providerResponse: stringifyResponse,
-							transactionId: transactionId,
-							externalTransactionId: externalTransactionId,
-						};
-						checkout.closePopUp();
-						toast.promise(
-							writeSale(dbInfo).then(() => {
-								setTimeout(() => setIsSuccessModalOpen(true), 300);
-								reset();
-								setRegID("");
+  return {
+    initialize: (
+      toast: {
+        promise: (
+          promise: Promise<unknown>,
+          options: { loading: string; success: string; error: string }
+        ) => void;
+      },
+      setIsSuccessModalOpen: (value: boolean) => void,
+      reset: () => void,
+      setDatePickerValue: (value: {
+        startDate: Date | null;
+        endDate: Date | null;
+      }) => void
+    ) => {
+      checkout.openModal({
+        purchaseInfo: purchaseInfo,
+        config: config,
+        callBacks: {
+          onInit: () => console.log("Iframe initialized: "),
+          onPaymentSuccess: (data) => {
+            console.log("Payment successful: ", data.data);
+            const response = data.data;
 
-								setDatePickerValue({
-									startDate: null,
-									endDate: null,
-								});
-							}),
-							{
-								loading: "Connecting you to Pentagon WiFi...",
-								success: "Registration complete!",
-								error: "Registration failed. Please try again.",
-							},
-						);
-					},
-					onPaymentFailure: (data) => {
-						console.log("Payment failed: ", data);
-					},
-					onLoad: () => {
-						console.log("Checkout has been loaded: ");
-					},
-					onFeesChanged: (fees) => {
-						console.log("Payment channel has changed: ", fees);
-					},
-					onResize: (size) => {
-						console.log("Iframe has been resized: ", size?.height);
-					},
-					onClose: () => {
-						const dbInfo: PendingRegistrationPayload = {
-							...paymentInfo,
-							regID: regID,
-							purchaseInfo: stringifyPurchaseInfo,
-							clientReference: clientReference,
-							providerResponse: "N/A",
-						};
+            const stringifyResponse = JSON.stringify(response);
+            const parseResponse = JSON.parse(response);
+            const transactionId = parseResponse.transactionId;
+            const externalTransactionId = parseResponse.externalTransactionId;
 
-						const response = writePendingRegistration(dbInfo);
-						response
-							.then((res) => {})
-							.catch((err) => console.log(err))
-							.finally(() => {});
-					},
-				},
-			});
-		},
-	};
+            const saleInfo: SalesPayload = {
+              ...checkoutInfo,
+              providerResponse: stringifyResponse,
+              transactionId: transactionId,
+              externalTransactionId: externalTransactionId,
+            };
+            checkout.closePopUp();
+            toast.promise(
+              writeSale(saleInfo).then(() => {
+                setTimeout(() => setIsSuccessModalOpen(true), 300);
+                reset();
+
+                setDatePickerValue({
+                  startDate: null,
+                  endDate: null,
+                });
+              }),
+              {
+                loading: "Connecting you to Pentagon WiFi...",
+                success: "Registration complete!",
+                error: "Registration failed. Please try again.",
+              }
+            );
+          },
+          onPaymentFailure: (data) => {
+            console.log("Payment failed: ", data);
+          },
+          onLoad: () => {
+            console.log("Checkout has been loaded: ");
+          },
+          onFeesChanged: (fees) => {
+            console.log("Payment channel has changed: ", fees);
+          },
+          onResize: (size) => {
+            console.log("Iframe has been resized: ", size?.height);
+          },
+          onClose: () => {
+            const response = writePendingRegistration(checkoutInfo);
+            response
+              .then((res) => {})
+              .catch((err) => console.log(err))
+              .finally(() => {});
+          },
+        },
+      });
+    },
+  };
 };
--- src/hooks/use-paystack.tsx
+++ src/hooks/use-paystack.tsx
@@ -1,121 +1,135 @@
-import type { PaymentInfo, PaystackSuccessReference, SalesPayload } from "@/lib/types";
-import { googleScriptUrl } from "@/lib/utils";
+import type {
+  PaymentInfo,
+  PaystackSuccessReference,
+  PendingPaymentPayload,
+  SalesPayload,
+  UserInfo,
+} from "@/lib/types";
+import { googleScriptUrl, writeRegistration } from "@/lib/utils";
 import { usePaystackPayment } from "react-paystack";
 import { v4 as uuidv4 } from "uuid";
 
-export const paystackPay = (paymentInfo: PaymentInfo, regID: string) => {
-	const reference = String(uuidv4());
-	const slicedReference = reference.slice(0, 8);
-	const clientReference = `PWT-${slicedReference}`;
+export const paystackPay = (userInfo: UserInfo) => {
+  const reference = String(uuidv4());
+  const slicedReference = reference.slice(0, 8);
+  const clientReference = `PWT-${slicedReference}`;
 
-	const config = {
-		reference: clientReference,
-		email: paymentInfo.email,
-		amount: paymentInfo.totalCost,
-		publicKey: import.meta.env.VITE_STACK_COLLECTIONS,
-		currency: import.meta.env.VITE_CURRENCY,
-		metadata: {
-			custom_fields: [
-				{
-					display_name: "Full Name",
-					variable_name: "fullName",
-					value: String(paymentInfo.fullName),
-				},
-				{
-					display_name: "Phone Number",
-					variable_name: "phoneNumber",
-					value: String(paymentInfo.phoneNumber),
-				},
-				{
-					display_name: "Subscription Plan",
-					variable_name: "subscriptionPlan",
-					value: String(paymentInfo.subscriptionPlan.toUpperCase()),
-				},
-			],
-		},
-	};
+  const config = {
+    reference: clientReference,
+    email: userInfo.email,
+    amount: userInfo.totalCost,
+    publicKey: import.meta.env.VITE_STACK_COLLECTIONS,
+    currency: import.meta.env.VITE_CURRENCY,
+    metadata: {
+      custom_fields: [
+        {
+          display_name: "Full Name",
+          variable_name: "fullName",
+          value: String(userInfo.fullName),
+        },
+        {
+          display_name: "Phone Number",
+          variable_name: "phoneNumber",
+          value: String(userInfo.phoneNumber),
+        },
+        {
+          display_name: "Subscription Plan",
+          variable_name: "subscriptionPlan",
+          value: String(userInfo.subscriptionPlan.toUpperCase()),
+        },
+      ],
+    },
+  };
 
-	const stringifyPurchaseInfo = JSON.stringify(config);
+  const stringifyPurchaseInfo = JSON.stringify(config);
 
-	const onSuccess = (
-		toast: {
-			promise: (
-				promise: Promise<unknown>,
-				options: { loading: string; success: string; error: string },
-			) => void;
-		},
-		setIsSuccessModalOpen: (value: boolean) => void,
-		reset: () => void,
-		setDatePickerValue: (value: { startDate: null; endDate: null }) => void,
-		setRegID: (value: string) => void,
-		reference: PaystackSuccessReference,
-	): void => {
-		console.log("Payment successful: ", reference);
-		const stringifyResponse = JSON.stringify(reference);
+  const onSuccess = (
+    toast: {
+      promise: (
+        promise: Promise<unknown>,
+        options: { loading: string; success: string; error: string }
+      ) => void;
+    },
+    setIsSuccessModalOpen: (value: boolean) => void,
+    reset: () => void,
+    setDatePickerValue: (value: { startDate: null; endDate: null }) => void,
+    reference: PaystackSuccessReference
+  ): void => {
+    console.log("Payment successful: ", reference);
+    const stringifyResponse = JSON.stringify(reference);
 
-		const purchaseInfo: SalesPayload = {
-			...paymentInfo,
-			regID: regID,
-			clientReference: clientReference,
-			purchaseInfo: stringifyPurchaseInfo,
-			providerResponse: stringifyResponse,
-			transactionId: reference.transaction,
-			externalTransactionId: null,
-		};
+    const purchaseInfo: SalesPayload = {
+      ...userInfo,
+      clientReference: clientReference,
+      purchaseInfo: stringifyPurchaseInfo,
+      providerResponse: stringifyResponse,
+      transactionId: reference.transaction,
+      externalTransactionId: null,
+    };
 
-		toast.promise(
-			fetch(googleScriptUrl, {
-				method: "POST",
-				mode: "no-cors",
-				body: JSON.stringify(purchaseInfo),
-				headers: {
-					"Content-Type": "application/json",
-				},
-			}).then(() => {
-				setTimeout(() => setIsSuccessModalOpen(true), 300);
-				reset();
-				setRegID("");
-				setDatePickerValue({
-					startDate: null,
-					endDate: null,
-				});
-			}),
-			{
-				loading: "Connecting you to Pentagon WiFi...",
-				success: "Registration complete!",
-				error: "Registration failed. Please try again.",
-			},
-		);
-	};
+    toast.promise(
+      fetch(googleScriptUrl, {
+        method: "POST",
+        mode: "no-cors",
+        body: JSON.stringify(purchaseInfo),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then(() => {
+        setTimeout(() => setIsSuccessModalOpen(true), 300);
+        reset();
+        setDatePickerValue({
+          startDate: null,
+          endDate: null,
+        });
+      }),
+      {
+        loading: "Connecting you to Pentagon WiFi...",
+        success: "Registration complete!",
+        error: "Registration failed. Please try again.",
+      }
+    );
+  };
 
-	const onClose = () => {};
-	const initializePayment = usePaystackPayment(config);
+  const onClose = () => {};
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const initializePayment = usePaystackPayment(config);
 
-	return {
-		initialize: (
-			toast: {
-				promise: (
-					promise: Promise<unknown>,
-					options: { loading: string; success: string; error: string },
-				) => void;
-			},
-			setIsSuccessModalOpen: (value: boolean) => void,
-			reset: () => void,
-			setDatePickerValue: (value: { startDate: null; endDate: null }) => void,
-			setRegID: (value: string) => void,
-		) => {
-			initializePayment({
-				onSuccess: (reference: PaystackSuccessReference) =>
-					onSuccess(
-						toast,
-						setIsSuccessModalOpen,
-						reset,
-						setDatePickerValue,
-						setRegID,
-						reference,
-					),
-				onClose,
-			});
-		},
-	};
+  const checkoutInfo: PendingPaymentPayload = {
+    ...userInfo,
+    purchaseInfo: stringifyPurchaseInfo,
+    clientReference: clientReference,
+  };
+
+  const response = writeRegistration(checkoutInfo);
+  response
+    .then((res) => {})
+    .catch((err) => console.log(err))
+    .finally(() => {});
+
+  return {
+    initialize: (
+      toast: {
+        promise: (
+          promise: Promise<unknown>,
+          options: { loading: string; success: string; error: string }
+        ) => void;
+      },
+      setIsSuccessModalOpen: (value: boolean) => void,
+      reset: () => void,
+      setDatePickerValue: (value: { startDate: null; endDate: null }) => void
+    ) => {
+      initializePayment({
+        onSuccess: (reference: PaystackSuccessReference) =>
+          onSuccess(
+            toast,
+            setIsSuccessModalOpen,
+            reset,
+            setDatePickerValue,
+            reference
+          ),
+        onClose,
+      });
+    },
+  };
 };
