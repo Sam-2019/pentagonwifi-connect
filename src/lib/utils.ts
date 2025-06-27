3c28b93d9f88fb23039c9e43f0da59a1816ccfbb
Sam <samtmartey@gmail.com>
2025-06-27 14:07:26 +0000

    refactor: update validation schema for improved name matching and formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -85,45 +85,42 @@ export const dataPlanOptions = [
 ];
 
 export const schema = yup
-  .object({
-    fullName: yup
-      .string()
-      .required("Name is required.")
-      .matches(
-        /^(?:[A-Za-z][A-Za-z'-]*\s+){1,}[A-Za-z][A-Za-z'-]*$/,
-        "Name is invalid."
-      ),
-    dateOfBirth: yup
-      .date()
-      .required("Date of birth is required.")
-      .typeError("Invalid date"),
-    phoneNumber: yup
-      .string()
-      .required("Phone number is required.")
-      .matches(/^(?:\+?\d{7,15}|0\d{9})$/, "Phone number is invalid"),
-    email: yup
-      .string()
-      .email()
-      .required("Email is required.")
-      .matches(
-        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-        "Email is invalid."
-      ),
-    blockCourt: yup.string().required("Block / Court is required."),
-    roomType: yup.string().required("Room Type is required"),
-    roomNumber: yup.string().required("Room number is required."),
-    subscriptionPlan: yup.string().required("Subscription plan is required"),
-    isCustodian: yup.bool().default(false).required("Custodian is required"),
-    userName: yup
-      .string()
-      .required("Username is required.")
-      .matches(
-        /^(?!.*__)(?!_)(?!.*_$)(?=.*[A-Za-z])(?=^[A-Za-z\d_]*\d{4}[A-Za-z\d_]*$)[A-Za-z\d_]+$/,
-        "Username is invalid."
-      ),
-    password: yup.string().required("Password is required."),
-  })
-  .required();
+	.object({
+		fullName: yup
+			.string()
+			.required("Name is required.")
+			.matches(/^[A-Za-z]+(?:\s[A-Za-z]{3,}){1,2}$/, "Name is invalid."),
+		dateOfBirth: yup
+			.date()
+			.required("Date of birth is required.")
+			.typeError("Invalid date"),
+		phoneNumber: yup
+			.string()
+			.required("Phone number is required.")
+			.matches(/^(?:\+?\d{7,15}|0\d{9})$/, "Phone number is invalid"),
+		email: yup
+			.string()
+			.email()
+			.required("Email is required.")
+			.matches(
+				/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+				"Email is invalid."
+			),
+		blockCourt: yup.string().required("Block / Court is required."),
+		roomType: yup.string().required("Room Type is required"),
+		roomNumber: yup.string().required("Room number is required."),
+		subscriptionPlan: yup.string().required("Subscription plan is required"),
+		isCustodian: yup.bool().default(false).required("Custodian is required"),
+		userName: yup
+			.string()
+			.required("Username is required.")
+			.matches(
+				/^(?!.*__)(?!_)(?!.*_$)(?=.*[A-Za-z])(?=^[A-Za-z\d_]*\d{4}[A-Za-z\d_]*$)[A-Za-z\d_]+$/,
+				"Username is invalid."
+			),
+		password: yup.string().required("Password is required."),
+	})
+	.required();
 
 export const topupSchema = yup
   .object({
