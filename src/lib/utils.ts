4a6ebef3b115eb3141fed32019855c4fc106fde2
Sam-2019 <samtmartey@gmail.com>
2025-10-03 11:04:01 +0000

    schema

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,3 @@
-import * as yup from "yup";
 import { twMerge } from "tailwind-merge";
 import type {
   SalesPayload,
@@ -10,16 +9,6 @@ import type {
   PendingRegistrationPayload,
 } from "./types";
 import { clsx, type ClassValue } from "clsx";
-
-export const dateOptions = {
-  year: "numeric" as const,
-  month: "2-digit" as const,
-  day: "2-digit" as const,
-  hour: "2-digit" as const,
-  minute: "2-digit" as const,
-  second: "2-digit" as const,
-  hour12: false,
-};
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -111,81 +100,6 @@ export const feedbackCategories = [
   { value: "Other", label: "Other" },
 ];
 
-export const schema = yup
-  .object({
-    fullName: yup
-      .string()
-      .required("Name is required.")
-      .matches(/^[A-Za-z]+(?:\s[A-Za-z]{3,}){1,2}$/, "Name is invalid."),
-    dateOfBirth: yup
-      .date()
-      .required("Date of birth is required.")
-      .typeError("Invalid date"),
-    phoneNumber: yup
-      .string()
-      .required("Phone number is required.")
-      .matches(/^(?:\+?\d{7,15}|0\d{9})$/, "Phone number is invalid"),
-    email: yup
-      .string()
-      .email()
-      .required("Email is required.")
-      .matches(
-        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-        "Email is invalid."
-      ),
-    blockCourt: yup.string().required("Block / Court is required."),
-    roomType: yup.string().required("Room Type is required"),
-    roomNumber: yup.string().required("Room number is required."),
-    subscriptionPlan: yup.string().required("Subscription plan is required"),
-    isCustodian: yup.bool().default(false).required("Custodian is required"),
-    userName: yup
-      .string()
-      .required("Username is required.")
-      .matches(
-        /^(?!.*__)(?!_)(?!.*_$)(?=.*[A-Za-z])(?=^[A-Za-z\d_]*\d{4}[A-Za-z\d_]*$)[A-Za-z\d_]+$/,
-        "Username is invalid."
-      ),
-    password: yup.string().required("Password is required."),
-  })
-  .required();
-
-export const topupSchema = yup
-  .object({
-    phoneNumber: yup
-      .string()
-      .required("Phone number is required.")
-      .matches(/^(?:\+?\d{7,15}|0\d{9})$/, "Phone number is invalid"),
-    email: yup
-      .string()
-      .email()
-      .required("Email is required.")
-      .matches(
-        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-        "Email is invalid."
-      ),
-    subscriptionPlan: yup.string().required("Subscription plan is required"),
-    userName: yup
-      .string()
-      .required("Username is required.")
-      .matches(
-        /^(?!.*__)(?!_)(?!.*_$)(?=.*[A-Za-z])(?=^[A-Za-z\d_]*\d{4}[A-Za-z\d_]*$)[A-Za-z\d_]+$/,
-        "Username is invalid."
-      ),
-  })
-  .required();
-
-export const feedbackSchema = yup.object({
-  fullName: yup
-    .string()
-    .required("Name is required.")
-    .matches(/^[A-Za-z]+(?:\s[A-Za-z]{3,}){1,2}$/, "Name is invalid."),
-  phoneNumber: yup
-    .string()
-    .required("Phone number is required.")
-    .matches(/^(?:\+?\d{7,15}|0\d{9})$/, "Phone number is invalid"),
-  category: yup.string().required("Category is required."),
-  comment: yup.string().required("Comment is required."),
-});
 export const hubtel = import.meta.env.VITE_HUBTEL;
 export const paystack = import.meta.env.VITE_PAYSTACK;
 export const auth = import.meta.env.VITE_AUTHORIZATION;
