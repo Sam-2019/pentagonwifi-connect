2b25655a93eccdffc03601836b984b6b3cd719ee
Sam-2019 <samtmartey@gmail.com>
2025-10-14 10:53:05 +0000

    update

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -49,7 +49,7 @@ export const registrationType = {
   },
   registration: {
     name: registration,
-    fee: 50,
+    fee: 100,
   },
   feedback: {
     name: feedback,
@@ -78,7 +78,7 @@ export const dataPlanOptions = [
     value: `Daily-(GHC ${planPrices.daily})`,
     label: `Daily (GHC ${planPrices.daily})`,
   },
-  // { value: `Weekly-(GHC ${planPrices.weekly})`, label: `Weekly (GHC ${planPrices.weekly})` },
+  { value: `Weekly-(GHC ${planPrices.weekly})`, label: `Weekly (GHC ${planPrices.weekly})` },
   // { value: `Monthly-(GHC ${planPrices.monthly})`, label: `Monthly (GHC ${planPrices.monthly})` }
 ];
 
